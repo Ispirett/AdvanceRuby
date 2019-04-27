b76b91f11d17bb2f40635e8a3dc592c9487a1ac8
Ispire <ispirett@gmail.com>
2019-04-26 21:29:05 -0400

    feat(bubble_sort_by): added bubble sort method

--- bubbleSort.rb
+++ bubbleSort.rb
@@ -1,6 +1,5 @@
-arr = [2, 3, 4, 65, 87, 9,1,6,767,43,2,32,54,6,575,645654,5]
 
-def BubbleSort(arr)
+def bubble_sort(arr)
   switch = true
   (0...(arr.length - 1)).each.with_index do |_item, _i|
     switch = false
@@ -14,6 +13,33 @@ def BubbleSort(arr)
     break if switch === false
   end
 end
-BubbleSort(arr)
+
+
+def bubble_sort_by(arr)
+  (0...(arr.length - 1)).each.with_index do |_item, _i|
+    switch = false
+    (0...(arr.length - _i - 1)).each.with_index do |_element, _j|
+
+      if arr[_j].length > arr[_j + 1].length
+        arr[_j],  arr[_j + 1] = arr[_j + 1] ,arr[_j]
+        switch = true
+      end
+    end
+    break if switch === false
+  end
+
+
+end
+
+arr = [2, 3, 4, 65, 87, 9,1,6,767,43,2,32,54,6,575,645654,5]
+str_arr = ["microverse", "i" ,"love"]
+
+
+#bubble sort
+bubble_sort(arr)
 p arr
 
+
+#bubble_sort_by
+bubble_sort_by(str_arr)
+p  str_arr
